ccc36b3b3799a6c6075bf332b9d17e6cfb130dae
The Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2024-01-28 00:17:08 +0530

    Time: 15 ms (24.02%), Space: 44.7 MB (14.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -8,7 +8,13 @@ class Solution {
         HashMap<Character, Integer> tMap = new HashMap<>();
 
         for (char c : s.toCharArray()) {
-            sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+            // sMap.put(c, sMap.getOrDefault(c, 0) + 1);
+            if(sMap.containsKey(c))
+            {
+                sMap.put(c,sMap.get(c)+1);
+            }
+            else
+                sMap.put(c,1);
         }
 
         for (char c : t.toCharArray()) {
