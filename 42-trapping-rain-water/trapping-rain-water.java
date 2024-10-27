9aa28a3f63bb19b1973033fc55b25cc49e7c076e
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2024-10-27 19:54:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (19.91%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,34 +1,57 @@
 class Solution {
     public int trap(int[] height) {
+
         int n=height.length;
-        int [] left_max=new int[n];
-        int [] right_max=new int[n];
-        int max=0;
+        // brute force method
+        // int [] left_max=new int[n];
+        // int [] right_max=new int[n];
+        // int max=0;
 
-        for(int i=0;i<n;i++){
-            if(height[i]>=max)
-            {
-                max=height[i];
-                left_max[i]=max;
-            }
-            else
-               left_max[i]=max;
-        }
-        max=0;
-        for(int i=n-1;i>=0;i--){
-            if(height[i]>=max)
-            {
-                max=height[i];
-                right_max[i]=max;
-            }
-            else
-               right_max[i]=max;
-        }
-        int water_trapped=0;
-        for(int i=0;i<n;i++)
-        {
-            water_trapped += Math.min(left_max[i], right_max[i]) - height   [i];
+        // for(int i=0;i<n;i++){
+        //     if(height[i]>=max)
+        //     {
+        //         max=height[i];
+        //         left_max[i]=max;
+        //     }
+        //     else
+        //        left_max[i]=max;
+        // }
+        // max=0;
+        // for(int i=n-1;i>=0;i--){
+        //     if(height[i]>=max)
+        //     {
+        //         max=height[i];
+        //         right_max[i]=max;
+        //     }
+        //     else
+        //        right_max[i]=max;
+        // }
+        // int water_trapped=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     water_trapped += Math.min(left_max[i], right_max[i]) - height[i];
 
+        // }
+        // return water_trapped;
+
+        // optimal approach
+
+        int l=0,left_max=0,r=n-1,right_max=0,water_trapped=0;
+        while(l<r){
+            if(height[l]<=height[r]){
+                if(height[l]>=left_max)
+                   left_max=height[l];
+                else
+                   water_trapped+=(left_max-height[l]);
+              l++;
+            }
+            else{
+                if(height[r]>=right_max)
+                   right_max=height[r];
+                else
+                   water_trapped+=(right_max-height[r]);
+              r--;
+            }
         }
         return water_trapped;
 
