1604f6010ac98b0e9098568bab88754fd1895dcd
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-02-04 12:41:15 +0530

    Time: 7 ms (63.72%) | Memory: 51.7 MB (50.51%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,11 +1,27 @@
 class Solution {
     public List<Integer> grayCode(int n) {
         
-        List<Integer> result = new ArrayList<>();
-        int totalNumbers = 1 << n;
-        for (int i = 0; i < totalNumbers; i++) {
-             result.add(i ^ (i >> 1));
+        // List<Integer> result = new ArrayList<>();
+        // int totalNumbers = 1 << n;
+        // for (int i = 0; i < totalNumbers; i++) {
+        //      result.add(i ^ (i >> 1));
+        // }
+        // return result;
+
+// brute force
+        List<Integer>solution= new ArrayList<>();
+        solution.add(0);
+        solution.add(1);
+        int curr=1;
+        for(int i=2;i<=n;i++)
+        {
+            curr=curr*2;
+            for(int j=solution.size()-1;j>=0;j--)
+            {
+                solution.add(curr+solution.get(j));
+            }
         }
-        return result;
+        return solution;
+
     }
 }
