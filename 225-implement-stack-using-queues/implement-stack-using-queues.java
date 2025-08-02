427addf59001dd3b0c37de948815759eb4ef6dc4
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-08-02 13:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (47.84%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -5,39 +5,47 @@ class MyStack {
         
     }
     
-    public void push(int x) {
+    public void push(int x) { //O(1)
+        // qe.add(x);
         qe.add(x);
+		for(int i=0;i<qe.size()-1;i++)
+		{
+		    qe.add(qe.remove());
+		}
     }
     
-    public int pop() {
+    public int pop() {  //O(n)
         if(empty())
 		{
 			return -1;
 		}
-		int count=0;
-		while(count<qe.size()-1)
-		{
-		   qe.add(qe.remove());
-		   count++;
-		}
-		return qe.remove();
+		// int count=0;
+		// while(count<qe.size()-1)
+		// {
+		//    qe.add(qe.remove());
+		//    count++;
+		// }
+		// return qe.remove();
+        return qe.remove();
+
     }
     
-    public int top() {
+    public int top() {   //O(n)
         
 		if(empty())
 		{ 
 			return -1;
 		}
-		int count=0;
-		while(count<qe.size()-1)
-		{
-		   qe.add(qe.remove());
-		   count++;
-		}
-	    int x= qe.peek();
-		qe.add(qe.remove());
-		return x;
+		// int count=0;
+		// while(count<qe.size()-1)
+		// {
+		//    qe.add(qe.remove());
+		//    count++;
+		// }
+	    // int x= qe.peek();
+		// qe.add(qe.remove());
+		// return x;
+        return qe.peek();
     }
     
     public boolean empty() {
