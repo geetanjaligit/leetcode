485efc0cefa160f1368bd129d443b89db1c18ff5
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-09-24 00:49:58 +0530

    Time: 38 ms (13.68%) | Memory: 45.4 MB (11.02%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int l=0,r=0,maxlen=0,maxf=0;
+        int n=s.length();
+        HashMap<Character,Integer>map=new HashMap<>();
+        while(r<n)
+        {
+            map.put(s.charAt(r),map.getOrDefault(s.charAt(r),0)+1);
+            maxf=Math.max(maxf,map.get(s.charAt(r)));
+            int changes=r-l+1-maxf;
+            if(changes>k)
+            {
+                map.put(s.charAt(l),map.get(s.charAt(l))-1);
+                l++;
+            }
+            if(changes<=k)
+            {
+                maxlen=Math.max(maxlen,r-l+1);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+}
