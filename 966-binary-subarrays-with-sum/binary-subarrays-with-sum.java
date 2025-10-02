2ae4fe12a60555510c94910d14c5593f4d6a7778
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-10-02 12:12:34 +0530

    Time: 4 ms (44.36%) | Memory: 47.5 MB (85.69%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,20 +1,64 @@
 class Solution {
-    public int numSubarraysWithSum(int[] nums, int goal) {
+    // public int numSubarraysWithSum(int[] nums, int goal) {
         
-        HashMap<Integer,Integer>map=new HashMap<>();
-        map.put(0,1);
+    //     HashMap<Integer,Integer>map=new HashMap<>();
+    //     map.put(0,1);
 
-        int preSum=0,count=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            preSum=preSum+nums[i];
-            int x=preSum-goal;
-            if(map.containsKey(x))
-            {
-                count=count+map.get(x);
-            }
-            map.put(preSum,map.getOrDefault(preSum,0)+1);
-        }
-        return count;
+    //     int preSum=0,count=0;
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         preSum=preSum+nums[i];
+    //         int x=preSum-goal;
+    //         if(map.containsKey(x))
+    //         {
+    //             count=count+map.get(x);
+    //         }
+    //         map.put(preSum,map.getOrDefault(preSum,0)+1);
+    //     }
+    //     return count;
+    // }
+
+    
+    public int numSubarraysWithSum(int[] nums, int goal)
+    {
+        
+        int count1=subarrayLessThanEqualGoal(nums,goal);
+        int count2=subarrayLessThanEqualGoalMinusOne(nums,goal-1);
+        return count1-count2;
     }
+    public int subarrayLessThanEqualGoal(int[] nums, int goal)
+    {
+        int l=0,r=0,sum=0,count1=0;
+        while(r<nums.length)
+        {
+            sum=sum+nums[r];
+            while(sum>goal)
+            {
+                sum=sum-nums[l];
+                l++;
+            }
+            count1=count1+(r-l+1);
+            r++;
+        }
+        return count1;
+    }
+    public int subarrayLessThanEqualGoalMinusOne(int[] nums, int goal)
+    {
+        if(goal<0)
+          return 0;
+        int l=0,r=0,sum=0,count2=0;
+        while(r<nums.length)
+        {
+            sum=sum+nums[r];
+            while(sum>goal)
+            {
+                sum=sum-nums[l];
+                l++;
+            }
+            count2=count2+(r-l+1);
+            r++;
+        }
+        return count2;
+    }
+
 }
