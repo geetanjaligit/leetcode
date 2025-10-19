6f8492e1958abb098f050d49d7533832667c2d5f
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-10-19 22:21:48 +0530

    Time: 5 ms (99.42%) | Memory: 45.4 MB (99.14%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int m=matrix.length;
         int n=matrix[0].length;
         int i=0,j=n-1;
-        while((i>=0 && i<=m-1) && (j>=0 && j<=n-1))
+        while(i<m && j>=0)
         {
             if(target==matrix[i][j])
             {
