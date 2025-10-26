8d2a15978f1f59307b70feb719891d46869ef3c2
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-10-26 13:50:56 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (70.20%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,41 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-        return new int[]{first, last};
+        return new int[]{firstPos(nums,target),lastPos(nums,target)};
     }
-
-    public int findFirst(int []nums,int target)
+    public int lastPos(int[]nums,int target)
     {
-        int left = 0, right = nums.length - 1;
-        int index = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
+        int ans=-1,l=0,u=nums.length-1;
+        while(l<=u)
+        {
+            int mid=l+(u-l)/2;
+            if(nums[mid]==target)
+            {
+               ans=mid;
+               l=mid+1;
             }
-            if (nums[mid] == target) {
-                index = mid;
-            }
+            else if(nums[mid]<target)
+               l=mid+1;
+            else 
+               u=mid-1;
         }
-        return index;
+        return ans;
     }
-
-    public int findLast(int []nums,int target)
+    public int firstPos(int []nums,int target)
     {
-         int left = 0, right = nums.length - 1;
-        int index = -1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] <= target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+        int ans=-1,l=0,u=nums.length-1;
+        while(l<=u)
+        {
+            int mid=l+(u-l)/2;
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                u=mid-1;
             }
-            if (nums[mid] == target) {
-                index = mid;
-            }
+            else if(nums[mid]<target)
+                l=mid+1;
+            else 
+                u=mid-1;
         }
-        return index;
+        return ans;
     }
 }
