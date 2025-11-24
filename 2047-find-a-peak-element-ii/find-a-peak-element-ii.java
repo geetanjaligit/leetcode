b6cdda20ce6d9aec81adb683b8f1e31aba9ede6d
Geetanjali <119747661+geetanjaligit@users.noreply.github.com>
2025-11-24 15:46:40 +0530

    Time: 0 ms (100.00%) | Memory: 115.7 MB (30.21%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.java
@@ -1,26 +1,33 @@
 class Solution {
-    public int[] findPeakGrid(int[][] matrix) {
-        int low=0;
-        int high=matrix[0].length-1;
-        while(low<=high)
+    public int[] findPeakGrid(int[][] mat) {
+        int n=mat.length,m=mat[0].length;
+        int l=0,u=m-1;
+        while(l<=u)
         {
-            int mid=(low+high)/2;
-            int max=matrix[0][mid];
-            int idx=0;
-            for(int i=1;i<matrix.length;i++)
-            {
-                if(max<matrix[i][mid])
-                {
-                    idx=i;
-                    max=matrix[i][mid];
-                }
-            }
-            int left=(mid-1>=0)?matrix[idx][mid-1]:-1;
-            int right=(mid+1<matrix[0].length)?matrix[idx][mid+1]:-1;
-            if(max>left && max>right) return new int[]{idx, mid};
-            else if(max>left) low=mid+1;
-            else high=mid-1;
+            int mid=l+(u-l)/2;
+            int row=maxElem(mat,n,mid);
+            int left_val=mid-1>=0?mat[row][mid-1]:-1;
+            int right_val=mid+1<m?mat[row][mid+1]:-1;
+            if(mat[row][mid]>left_val && mat[row][mid]>right_val)
+               return new int[] {row,mid};
+            else if(mat[row][mid]<left_val)
+               u=mid-1;
+            else if(mat[row][mid]<right_val)
+               l=mid+1;
         }
-        return new int[]{-1, -1};
+        return  new int[]{-1,-1};
+    }
+    public int maxElem(int [][]mat,int n,int mid)
+    {
+        int max=Integer.MIN_VALUE,maxInd=-1;
+        for(int i=0;i<n;i++)
+        {
+            if(mat[i][mid]>max)
+            {
+                max=mat[i][mid];
+                maxInd=i;
+            }
+        }
+        return maxInd;
     }
 }
